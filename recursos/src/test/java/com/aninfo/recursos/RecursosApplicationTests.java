3361d7b74066fbe26b887370c642e65fff1433e3
Lautaro Fritz <lautarofritz2@gmail.com>
2021-02-11 23:28:00 -0300

    primer commit, agarra el json del sistema externo

---
+++ recursos/src/test/java/com/aninfo/recursos/RecursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aninfo.recursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
