735e0521f7af268b75b447629b9b7846e88e5b99
Maxi Romero <mromerov@fi.uba.ar>
2021-02-13 13:57:26 -0300

    Se agregan archivos en package hours olvidados

--- recursos-backend/src/main/java/com/aninfo/hours/repository/HoursRepository.java
+++ recursos-backend/src/main/java/com/aninfo/hours/repository/HoursRepository.java
@@ -1,4 +1,13 @@
 package com.aninfo.hours.repository;
 
+import com.aninfo.hours.model.Hours;
+
+import java.util.Vector;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+
 public class HoursRepository {
 }
--- recursos-backend/src/main/java/com/aninfo/hours/service/HoursService.java
+++ recursos-backend/src/main/java/com/aninfo/hours/service/HoursService.java
@@ -1,4 +1,10 @@
 package com.aninfo.hours.service;
 
+import com.aninfo.hours.model.Hours;
+import com.aninfo.hours.repository.HoursRepository;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Vector;
+
 public class HoursService {
 }
